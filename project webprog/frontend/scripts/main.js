f2b54dc1eeccd0688c79387e7ce6947cf9140220
Michael Yan Petra <535189102+MichaelYanPetra@users.noreply.github.com>
2020-09-12 19:27:57 +0700

    update CSS+ Item.html

--- project webprog/frontend/scripts/main.js
+++ project webprog/frontend/scripts/main.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
     document.getElementById("navbarBot").style.bottom = "0";
   } else {
     document.getElementById("navbarBot").style.bottom = "-100px";
