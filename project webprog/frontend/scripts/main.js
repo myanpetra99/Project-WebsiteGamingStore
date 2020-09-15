1717a81c451e12a0154ebb1cfb8830b0ef22f769
Michael Yan Petra <535189102+MichaelYanPetra@users.noreply.github.com>
2020-09-15 12:26:17 +0700

    Cart-finished + added WA API confrimation (manual)

--- project webprog/frontend/scripts/main.js
+++ project webprog/frontend/scripts/main.js
@@ -7,3 +7,57 @@ function scrollFunction() {
     document.getElementById("navbarBot").style.bottom = "-100px";
   }
 }
+
+
+//All about cart
+function increment (obj) {
+  var count = parseInt(
+    $(obj)
+      .parent()
+      .parent()
+      .find('input')
+      .val()
+  )
+  $(obj)
+    .parent()
+    .parent()
+    .find('input')
+    .val(count + 1)
+
+
+    $(".btn-minus").prop('disabled', false);
+}
+
+
+
+
+function decrement (obj) {
+  var count = parseInt(
+    $(obj)
+      .parent()
+      .find('input')
+      .val()
+  )
+  if (count != 1) {
+    $(obj)
+      .parent()
+      .parent()
+      .find('input')
+      .val(count - 1)
+     
+  }
+
+}
+
+
+function check(){
+    var x = $( "input.adults" ).val();
+
+    x.chil
+
+    if(x==1){
+      $(".btn-minus").prop('disabled', true);
+    }
+}
+
+
